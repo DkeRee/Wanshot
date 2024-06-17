38616a62da32a8d4e629aebe048be1c9437b132d
Daniel Ke <dkeree1@gmail.com>
2024-06-17 12:43:20 -0700

    another slight tweak

--- js/introPack/text.js
+++ js/introPack/text.js
@@ -14,7 +14,7 @@ const mineTextThree = "You can shoot a mine to detonate.";
 
 const addTextOne = "For additional information and credits click on the button";
 const addTextTwo = "at the top left corner of the page. Recommended browsers";
-const addTextThree = "for playing Wankle are Chrome and Microsoft Edge. Other browsers";
+const addTextThree = "for playing Wanshot are Chrome and Microsoft Edge. Other browsers";
 const addTextFour = "may increase lag and disrupt gameplay. This game is open source and";
 const addTextFive = "is available on Github.";
 
